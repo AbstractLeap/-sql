777ca2517665043c2f3da87ff76d1e8427b19dbf
Mark Jerzykowski <mark@polylytics.com>
2021-03-07 19:01:52 +0000

    Dictionaries with complex keys supported in newtonsoft.json

--- Leap.Data.JsonNet/JsonNetFieldSerializer.cs
+++ Leap.Data.JsonNet/JsonNetFieldSerializer.cs
@@ -1,8 +1,15 @@
 namespace Leap.Data.JsonNet {
+    using System.Collections.Generic;
+
     using Newtonsoft.Json;
 
     public class JsonNetFieldSerializer : Serializer {
         public JsonNetFieldSerializer()
-            : base(new JsonSerializerSettings { ContractResolver = new FieldsOnlyContractResolver(), TypeNameHandling = TypeNameHandling.Auto }) { }
+            : base(
+                new JsonSerializerSettings {
+                    ContractResolver = new FieldsOnlyContractResolver(),
+                    TypeNameHandling = TypeNameHandling.Auto,
+                    Converters       = new List<JsonConverter> { new ComplexKeyDictionaryConverter() }
+                }) { }
     }
 }
