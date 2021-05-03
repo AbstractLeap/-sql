b98bfcd324e1ad39f2dd23765b04f5fe75c8b869
Mark Jerzykowski <mark@polylytics.com>
2021-05-03 22:24:21 +0100

    Fix issue with json fields initialiser duplicating protected fields

--- Leap.Data.JsonNet/FieldsOnlyContractResolver.cs
+++ Leap.Data.JsonNet/FieldsOnlyContractResolver.cs
@@ -1,6 +1,7 @@
 ﻿namespace Leap.Data.JsonNet {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Runtime.Serialization;
 
@@ -10,15 +11,18 @@
     public class FieldsOnlyContractResolver: DefaultContractResolver
     {
         protected override List<MemberInfo> GetSerializableMembers(Type objectType) {
-            var members = new List<MemberInfo>();
-            members.AddRange(objectType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic));
+            // See Remarks at https://docs.microsoft.com/en-us/dotnet/api/system.type.getfields?view=net-5.0
+            var members = new HashSet<MemberInfo>();
+            members.UnionWith(objectType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic));
             var baseType = objectType.BaseType;
             while (baseType != null && baseType != typeof(object)) {
-                members.AddRange(baseType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)); // NOTE we don't get the public ones here as they're already returned above.
+                // NOTE we don't get the public ones here as they're already returned above.
+                // but we do get protected ones here as well, but they're already returned above as well so we have to filter them out
+                members.UnionWith(baseType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Where(f => !f.IsFamily));
                 baseType = baseType.BaseType;
             }
 
-            return members;
+            return members.ToList();
         }
 
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization) {
