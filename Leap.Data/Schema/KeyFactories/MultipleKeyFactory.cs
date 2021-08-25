3c76a8560703654b6aaab1b926ac527596a2ee8f
Mark Jerzykowski <mark@abstractleap.com>
2021-08-25 15:37:21 +0100

    Actual nested keys

--- Leap.Data/Schema/KeyFactories/MultipleKeyFactory.cs
+++ Leap.Data/Schema/KeyFactories/MultipleKeyFactory.cs
@@ -1,6 +1,7 @@
 ﻿namespace Leap.Data.Schema.KeyFactories {
     using System;
     using System.Linq;
+    using System.Reflection;
 
     using Leap.Data.Schema.Columns;
     using Leap.Data.Utilities;
@@ -15,6 +16,8 @@
             var lookup = valuePaths.ToDictionary(i => i.Item2, i => this.rootNode);
             var maxMemberAccessorsLength = valuePaths.Max(p => p.MemberAccessors.Length);
             while (i < maxMemberAccessorsLength) {
+                MemberInfo previousMemberAccessor = null;
+                Node previousNode = null;
                 foreach (var path in valuePaths) {
                     if (i <= path.MemberAccessors.Length - 1) {
                         var parent = lookup[path.Item2];
@@ -22,9 +25,17 @@
                             parent.Add(new Leaf(path.Item2));
                         }
                         else if (i < path.MemberAccessors.Length - 1) {
-                            var node = new Node(path.MemberAccessors[i].PropertyOrFieldType());
-                            parent.Add(node);
-                            lookup[path.Item2] = node;
+                            var memberAccessor = path.MemberAccessors[i];
+                            if (previousMemberAccessor == memberAccessor) {
+                                lookup[path.Item2] = previousNode;
+                            }
+                            else {
+                                var node = new Node(memberAccessor.PropertyOrFieldType());
+                                parent.Add(node);
+                                lookup[path.Item2]     = node;
+                                previousMemberAccessor = memberAccessor;
+                                previousNode           = node;
+                            }
                         }
                     }
                 }
