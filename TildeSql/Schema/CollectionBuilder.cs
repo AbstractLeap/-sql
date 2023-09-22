91fa2b841bcdd7ff9d9c6e84679360a5bf06e7d2
Mark Jerzykowski <mark@abstractleap.com>
2023-09-22 13:28:35 +0100

    Better primary key column resolution in CollectionBuilder - (it works with inheritance)

--- TildeSql/Schema/CollectionBuilder.cs
+++ TildeSql/Schema/CollectionBuilder.cs
@@ -1,7 +1,10 @@
 ﻿namespace TildeSql.Schema {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
+
+    using TildeSql.Utilities;
 
     public class CollectionBuilder<TEntity> {
         private readonly SchemaBuilder schemaBuilder;
@@ -29,13 +32,21 @@
         }
 
         public SchemaBuilder PrimaryKey(params string[] memberNames) {
-            var members = typeof(TEntity).GetMembers(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).Where(m => memberNames.Contains(m.Name)).ToArray();
+            var members = new List<MemberInfo>();
+            var classHierarchy = typeof(TEntity).GetClassHierarchy();
+            foreach (var type in classHierarchy) {
+                var matchingMembers = type.GetMembers(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                                          .Where(m => memberNames.Contains(m.Name) && members.All(existing => existing.Name != m.Name))
+                                          .ToArray();
+                members.AddRange(matchingMembers);
+            }
+
             var missingMembers = memberNames.Where(name => members.All(m => m.Name != name)).ToArray();
             if (missingMembers.Any()) {
                 throw new ArgumentOutOfRangeException($"Unable to find members named {string.Join(", ", missingMembers)}");
             }
 
-            this.schemaBuilder.AddKeyMembers(typeof(TEntity), members);
+            this.schemaBuilder.AddKeyMembers(typeof(TEntity), members.ToArray());
             return this.schemaBuilder;
         }
     }
