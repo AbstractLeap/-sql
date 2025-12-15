c80a015be13613cdce584c4ce7371870cac3bcb8
Mark Jerzykowski <mark@abstractleap.com>
2025-12-15 15:14:53 +0000

    Fix index migration issue where non indexed are indexed

--- TildeSql.SqlMigrations/SchemaExtensions.cs
+++ TildeSql.SqlMigrations/SchemaExtensions.cs
@@ -18,27 +18,31 @@
                                        return new Table {
                                            Name   = t.GetTableName(),
                                            Schema = t.GetSchemaName(),
-                                           Columns = t.Columns.Select(
-                                                          c => new Column {
-                                                              Name            = c.Name,
-                                                              Type            = c is DocumentColumn ? typeof(Json) : c.Type,
-                                                              IsPrimaryKey    = c is KeyColumn,
-                                                              IsIdentity      = (c is KeyColumn && t.IsKeyComputed) || c is GenericColumn { IsIdentity: true },
-                                                              IsComputed      = c is ComputedColumn or GenericColumn { IsComputed: true } or GenericColumn { IsIdentity: true },
-                                                              IsPersisted     = c is ComputedColumn { Persisted: true },
-                                                              ComputedFormula = c is ComputedColumn computed ? computed.Formula : null,
-                                                              Size = c switch {
-                                                                  DocumentTypeColumn => 255,
-                                                                  GenericColumn genericColumn => genericColumn.Size,
-                                                                  _ => null
-                                                              },
-                                                              Precision  = c is GenericColumn genericColumn1 ? genericColumn1.Precision : null,
-                                                              IsNullable = c is GenericColumn { IsNullable: true }
-                                                          })
-                                                      .ToList(),
-                                           Indexes = t.Columns.OfType<ComputedColumn>()
-                                                      .Select(c => new Index { Name = $"idx_{c.Name}", Columns = new List<string> { c.Name } })
-                                                      .ToList()
+                                           Columns =
+                                               t.Columns.Select(c =>
+                                                    new Column {
+                                                        Name            = c.Name,
+                                                        Type            = c is DocumentColumn ? typeof(Json) : c.Type,
+                                                        IsPrimaryKey    = c is KeyColumn,
+                                                        IsIdentity      = (c is KeyColumn && t.IsKeyComputed) || c is GenericColumn { IsIdentity: true },
+                                                        IsComputed      = c is ComputedColumn or GenericColumn { IsComputed: true } or GenericColumn { IsIdentity: true },
+                                                        IsPersisted     = c is ComputedColumn { Persisted: true },
+                                                        ComputedFormula = c is ComputedColumn computed ? computed.Formula : null,
+                                                        Size =
+                                                            c switch {
+                                                                DocumentTypeColumn => 255,
+                                                                GenericColumn genericColumn => genericColumn.Size,
+                                                                _ => null
+                                                            },
+                                                        Precision  = c is GenericColumn genericColumn1 ? genericColumn1.Precision : null,
+                                                        IsNullable = c is GenericColumn { IsNullable: true }
+                                                    })
+                                                .ToList(),
+                                           Indexes =
+                                               t.Columns.OfType<ComputedColumn>()
+                                                .Where(c => c.Indexed)
+                                                .Select(c => new Index { Name = $"idx_{c.Name}", Columns = new List<string> { c.Name } })
+                                                .ToList()
                                        };
                                    }));
         }
